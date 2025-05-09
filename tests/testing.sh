ae3320d0faa383ca3b67e6cc030c0d406ae8f544
jvsguerra <jvsguerra@gmail.com>
2025-05-09 12:33:38 -0300

    Update workflow

--- tests/testing.sh
+++ tests/testing.sh
@@ -2,19 +2,19 @@
 cd "$(dirname "$0")"
 
 # Standard mode
-parKVFinder ../input/1FMO.pdb
+../parKVFinder ../input/1FMO.pdb
 
 # Change probe out
-parKVFinder ../input/1FMO.pdb -o 8.0
+../parKVFinder ../input/1FMO.pdb -o 8.0
 
 # Ligand mode
-parKVFinder ../input/1FMO.pdb -L ../input/ligs_1FMO.pdb
+../parKVFinder ../input/1FMO.pdb -L ../input/ligs_1FMO.pdb
 
 # Custom box mode
-parKVFinder ../input/1FMO.pdb -B --custom_box ../input/1FMO.box.KVFinder.in
+../parKVFinder ../input/1FMO.pdb -B --custom_box ../input/1FMO.box.KVFinder.in
 
 # Residue box mode
-parKVFinder ../input/1FMO.pdb -B --residues_box ../input/1FMO.residues.KVFinder.in 
+../parKVFinder ../input/1FMO.pdb -B --residues_box ../input/1FMO.residues.KVFinder.in 
 
 # Run from parameters.toml
-parKVFinder
+../parKVFinder
